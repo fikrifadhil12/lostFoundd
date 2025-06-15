d5be071286184533b98c3a985fddcc172e6a80cb
fikrifadhil12 <127014590+fikrifadhil12@users.noreply.github.com>
2025-06-15 15:51:20 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  // Tempel config object dari Firebase Console
-  apiKey: "AIzaSyDe2FJbd4Ty8Sg5XFmS00imgqJvYTbpEQ0",
-  authDomain: "lost-found-61d0c.firebaseapp.com",
-  projectId: "lost-found-61d0c",
-  storageBucket: "lost-found-61d0c.firebasestorage.app",
-  messagingSenderId: "247315226129",
-  appId: "1:247315226129:web:ad0cbd295fcc85fc49dbb0",
-  measurementId: "G-Y6RDNDJR60",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
